e73e2de08e0cd7e39e367e32f87268df3dd6cfb6
apantelakis <apantelakis@athtech.gr>
2025-01-11 15:03:43 +0200

    Add files via upload
    trimmed unnecessary spaces

--- other/script.js
+++ other/script.js
@@ -8,14 +8,17 @@ submitButton.addEventListener("click", () => {
        let commentInput = document.getElementById("commentInput")
        const newComment = document.createElement("div");
        newComment.className = "commentClass"
-       newComment.innerHTML = commentInput.value;
+       newComment.innerHTML = commentInput.value.trim(); // Trim unnecessary spaces
 
-       const date = new Date;
-       const dateElement = document.createElement("div");
-       dateElement.textContent = date;
-       dateElement.className = "dateClass"
+       // Checks if a comment is empty, if it is then it doesnt add it
+       if (commentInput.value.trim() != "") {
+              const date = new Date;
+              const dateElement = document.createElement("div");
+              dateElement.textContent = date;
+              dateElement.className = "dateClass"
 
-       comments.appendChild(dateElement);
-       comments.appendChild(newComment);
+              comments.appendChild(dateElement);
+              comments.appendChild(newComment);
+       }
 });
 
